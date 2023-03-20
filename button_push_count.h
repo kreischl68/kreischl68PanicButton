742c9a89cd482721a5cbdbcc6fb42e06060aacaf
kreischl68 <126209627+kreischl68@users.noreply.github.com>
2023-03-20 16:24:05 -0500

    Added note
    added line 13:  Karl was here kreischl68panicbutton

---
+++ button_push_count.h
@@ -0,0 +1,6 @@
+#ifndef BUTTON_PUSH_COUNT_H
+#define BUTTON_PUSH_COUNT_H
+
+extern volatile int button_push_count;
+
+#endif // BUTTON_PUSH_COUNT_H
---
+++ neopixel_strip.h
@@ -0,0 +1,8 @@
+#ifndef NEOPIXEL_STRIP_H
+#define NEOPIXEL_STRIP_H
+
+#include <Adafruit_NeoPixel.h>
+
+extern Adafruit_NeoPixel strip;
+
+#endif // NEOPIXEL_STRIP_H
